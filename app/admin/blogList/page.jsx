77bb035e452c5e34dcc88934281fb0f9d9d76b4c
Francesco Matteo Depalma <157421031+FDMDeveloper@users.noreply.github.com>
2024-07-08 10:53:53 +0200

    Update

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,64 +1,74 @@
-'use client'
+'use client'; // Optional directive for client-side components in Next.js
 
-import BlogTableItem from '@/Components/adminComponents/BlogTableItem'
+import BlogTableItem from '@/Components/adminComponents/BlogTableItem';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
-const page = () => {
+// Wrap the component definition in a React functional component
+function ManageBlogs() { // Capitalize the function name
 
-    const [blogs,setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([]); // State variable for blog data
 
-    const fetchBlogs = async () => {
-        const response = await axios.get('/api/blog');
-        setBlogs(response.data.blogs);
-    }
+  const fetchBlogs = async () => {
+    const response = await axios.get('/api/blog');
+    setBlogs(response.data.blogs);
+  };
 
-    const deleteBlog = async (mongoId) => {
-        const response = await axios.delete('/api/blog',{
-            params:{
-                id:mongoId
-            }
-        })
-        toast.success(response.data.msg);
-        fetchBlogs();
-    }
+  const deleteBlog = async (mongoId) => {
+    const response = await axios.delete('/api/blog', {
+      params: {
+        id: mongoId,
+      },
+    });
+    toast.success(response.data.msg);
+    fetchBlogs(); // Refetch blogs after deletion
+  };
 
-    useEffect(()=>{
-        fetchBlogs()
-    },[])
-
+  useEffect(() => {
+    fetchBlogs(); // Fetch blogs on component mount
+  }, []); // Empty dependency array to run the effect only once
 
   return (
     <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
-      <h1>All blogs</h1>
+      <h1>All Blogs</h1>
       <div className='relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'>
         <table className='w-full text-sm text-gray-500'>
-            <thead className='text-sm text-gray-700 text-left uppercase bg-gray-50'>
-                <tr>
-                    <th scope='col' className='hidden sm:block px-6 py-3'>
-                        Author name
-                    </th>
-                    <th scope='col' className='px-6 py-3'>
-                        Blog Title
-                    </th>
-                    <th scope='col' className='px-6 py-3'>
-                        Date
-                    </th>
-                    <th scope='col' className='px-6 py-3'>
-                        Action
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {blogs.map((item,index)=>{
-                    return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date} deleteBlog={deleteBlog} />
-                })}
-            </tbody>
+          <thead className='text-sm text-gray-700 text-left uppercase bg-gray-50'>
+            <tr>
+              <th scope='col' className='hidden sm:block px-6 py-3'>
+                Author Name
+              </th>
+              <th scope='col' className='px-6 py-3'>
+                Blog Title
+              </th>
+              <th scope='col' className='px-6 py-3'>
+                Date
+              </th>
+              <th scope='col' className='px-6 py-3'>
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {blogs.map((item, index) => {
+              return (
+                <BlogTableItem
+                  key={index}
+                  mongoId={item._id}
+                  title={item.title}
+                  author={item.author}
+                  authorImg={item.authorImg}
+                  date={item.date}
+                  deleteBlog={deleteBlog}
+                />
+              );
+            })}
+          </tbody>
         </table>
       </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default ManageBlogs; // Export the component
